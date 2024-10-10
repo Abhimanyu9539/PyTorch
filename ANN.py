ce54be8937620aafcaf016c7e50f809f95ece429
Abhimanyu9539 <iabhimanyu31@gmail.com>
2024-10-10 22:50:23 +0530

    PyTorch Tensor

--- ANN.py
+++ ANN.py
@@ -1,1 +1,12 @@
-import matplotlib
+import torch
+import torch.nn as nn
+
+# Assign inputs to the neuron
+inputs = torch.tensor([3.0, 2.0, -1.0])
+
+# Create neuron with 3 feature and 1 output
+neuron = nn.Linear(in_features=3, out_features=1)
+output = neuron(inputs)
+
+# Print output
+print(output)
